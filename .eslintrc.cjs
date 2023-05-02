216a60db2143da3114488d2c0a5bd85402faef5a
Jinha Hwang <pn_jhhwang@humaxit.com>
2023-05-02 18:25:33 +0900

    chore: 린트 룰 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,6 +10,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: [
+    'react',
     'react-refresh',
     // module import 관련 룰을 활성화시키기 위한 플러그인
     'import',
@@ -27,9 +28,30 @@ module.exports = {
     },
   },
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'no-unused-vars': 'error',
+    'no-unsafe-optional-chaining': [
+      'error',
+      { disallowArithmeticOperators: true },
+    ],
 
     // turn on errors for missing imports
     'import/no-unresolved': 'error',
+    'import/no-cycle': 'warn',
+
+    'react-refresh/only-export-components': 'warn',
+
+    // <tag attribute={"text"} /> 와 같은 구문을 <tag attribute="text" /> 으로 사용하라는 rule, (fixable)
+    'react/jsx-curly-brace-presence': [
+      'warn',
+      { props: 'never', children: 'never' },
+    ],
   },
+  overrides: [
+    {
+      files: ['**/*.stories.*'],
+      rules: {
+        'import/no-anonymous-default-export': 'off',
+      },
+    },
+  ],
 }
