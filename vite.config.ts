4075f1d64293865ebceb46e41b904f4de149ba3d
Jinha Hwang <pn_jhhwang@humaxit.com>
2023-05-04 09:35:42 +0900

    chore: eslint를 사용하기에 ts 전용 린트 일부 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  cacheDir: 'node_modules/.vite/vcp-admin-ui',
+  // server: {
+  //   port: 4200,
+  //   host: 'localhost',
+  // },
+  // preview: {
+  //   port: 4300,
+  //   host: 'localhost',
+  // },
   plugins: [react()],
   resolve: {
     alias: [
@@ -18,6 +27,11 @@ export default defineConfig({
     ],
   },
   test: {
+    globals: true,
+    cache: {
+      dir: 'node_modules/.vitest',
+    },
     environment: 'happy-dom', // or 'jsdom', 'node'
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 })
